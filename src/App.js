f01e4ea66cab8675d5df35b860f554993e36f43b
fkhjoy <khjoyfoysal@gmail.com>
2021-08-16 02:43:31 +0600

    submit task partial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask';
 
 
 function App() {
@@ -41,6 +42,7 @@ function App() {
   return (
     <div className="container">
       <Header text="Task Tracker"/>
+      <AddTask />
       {tasks.length > 0 ? (<Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No tasks to show')}
       <h1>Hello React!</h1>
     </div>
